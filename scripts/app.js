c3a2481406a0cca7a4f11e041c2629bb5c22b46d
rdodev <rubenoz+github@gmail.com>
2013-04-05 23:14:45 -0400

    full app

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var jsformationApp = angular.module('jsformationApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
